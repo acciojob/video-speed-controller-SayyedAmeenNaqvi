429cdfb191a3a2cbfdeb8631607a007b8f9bdd5a
Sayyed Ameen Naqvi <sayyedameennaqvi0430@gmail.com>
2025-10-02 12:10:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Select elements
-const video = document.querySelector('.viewer');
+const video = document.querySelector('.player__video');
 const toggle = document.querySelector('.toggle');
 const volume = document.querySelector('input[name="volume"]');
 const playbackRate = document.querySelector('input[name="playbackRate"]');
@@ -7,46 +6,38 @@ const skipButtons = document.querySelectorAll('[data-skip]');
 const progress = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress__filled');
 
-// Play / Pause toggle
 function togglePlay() {
   video[video.paused ? 'play' : 'pause']();
 }
 
-// Update play/pause button icon
 function updateButton() {
   toggle.textContent = video.paused ? '►' : '❚ ❚';
 }
 
-// Skip forward/backward
 function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
-// Handle volume and playback speed sliders
 function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
-// Update progress bar as video plays
 function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
-  progressBar.style.flexBasis = `${percent}%`;
+  progressBar.style.flexBasis = ${percent}%;
 }
 
-// Scrub video to clicked position
 function scrub(e) {
   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
 }
 
-// Event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
-
 volume.addEventListener('change', handleRangeUpdate);
 volume.addEventListener('mousemove', handleRangeUpdate);
 playbackRate.addEventListener('change', handleRangeUpdate);
@@ -54,7 +45,6 @@ playbackRate.addEventListener('mousemove', handleRangeUpdate);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
 
-// Progress bar scrubbing
 let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
